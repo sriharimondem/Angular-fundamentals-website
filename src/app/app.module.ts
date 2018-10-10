d96a2e373c1ea6f939850173476b51831fcab6d1
srihari mondem <srihari.mondem@suresby.net>
2018-10-10 15:48:45 +0530

    Angular application including services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/nav-bar.component';
+import { EventService } from './events/shared/event.service';
+import {ToastrService} from './common/toastr.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventService, ToastrService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit, Input} from '@angular/core';
+import { EventService } from '../events/shared/event.service';
+import {ToastrService} from '../common/toastr.service';
+
+
+@Component({
+    selector: 'app-events-list',
+    template: `
+    <div>
+    <h1>Upcoming angular events</h1>
+    <hr>
+        <div class='row'>
+            <div *ngFor='let event of events' class='col-md-5'>
+                <app-event-thumbnail (click)='handleThumbnailClick(event.name)' [event]='event'></app-event-thumbnail>
+            </div>
+        </div>
+    </div>`
+})
+
+export class EventsListComponent implements OnInit {
+    events: any = [];
+    constructor(private eventService: EventService, private toastr: ToastrService) {}
+    ngOnInit() {
+        this.events = this.eventService.getEvents();
+    }
+    handleThumbnailClick(eventName) {
+        this.toastr.info(eventName);
+    }
+}
