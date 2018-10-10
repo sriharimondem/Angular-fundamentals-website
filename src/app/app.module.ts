07ccb84c06362c258d2e95b7382e1af69af79ad3
srihari mondem <srihari.mondem@suresby.net>
2018-10-10 18:43:54 +0530

    Routing enabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { EventService } from './events/shared/event.service';
 import {ToastrService} from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './error/404.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { appRoutes } from './routes';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,13 @@
 import {Routes} from '@angular/router';
 import {EventsListComponent} from './events/events-list.component';
 import {EventDetailsComponent} from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './error/404.component';
 
 export const appRoutes: Routes = [
+    {path: 'events/new', component: CreateEventComponent},
     {path: 'events', component: EventsListComponent},
     {path: 'events/:id', component: EventDetailsComponent},
+    {path: '404', component: Error404Component},
     {path: '', redirectTo: '/events', pathMatch: 'full'}
 ];
